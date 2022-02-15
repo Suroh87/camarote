9825a603941869e9e8ee883cd039ac19df66fb78
Suroh87 <49732289+Suroh87@users.noreply.github.com>
2022-02-15 11:58:33 +0100

    Base App

---
+++ App gestor barcos/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Gestor de cargamento</title>
+	<link rel="stylesheet" type="text/css" href="estilos/estilos.css">
+	<script src="javascript/descarga.js"></script>
+	<script src="javascript/interfaz.js"></script>
+
+</head>
+<body>
+
+</body>
+</html>
